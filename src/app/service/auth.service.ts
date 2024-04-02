3e54612b5deb93622e18c40f37c7ca578e170485
tungdao <tung1602002@gmail.com>
2024-04-02 20:29:24 +0700

    fix routing admin

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -4,15 +4,21 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
-
+  private loggedIn: boolean = false;
   constructor() { }
 
   login(username: string, pass: string) {
     if( username === 'tungdao' && pass === '123456') {
+      this.loggedIn = true;
       return 200;
     }
     else {
+      this.loggedIn = false;
       return 403;
     }
+  }
+
+  isLoggedIn(): boolean {
+    return this.loggedIn;
   }
 }
---
+++ src/app/service/guard.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service'; // Giả sử AuthService là dịch vụ xác thực
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (this.authService.isLoggedIn()) {
+      return true; // Cho phép truy cập nếu người dùng đã đăng nhập
+    } else {
+      // Nếu chưa đăng nhập, điều hướng đến trang đăng nhập
+      return this.router.navigate(['login']);
+    }
+  }
+}
