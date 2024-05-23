5955abb10fbd95cd32b110cf889f5321e13a46d0
tungdao <tung1602002@gmail.com>
2024-05-24 00:25:54 +0700

    fix giao dien sidenav va navbar

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,8 +3,6 @@ import {AdminComponent} from "./admin.component";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {CommonModule} from "@angular/common";
 import {AdminRoutingModule} from "./admin-routing.module";
-import { AdminNavbarComponent } from './admin-navbar/admin-navbar.component';
-import { AdminSidenavComponent } from './admin-sidenav/admin-sidenav.component';
 import { LeaguageComponent } from './leaguage/leaguage.component';
 import { TeamComponent } from './team/team.component';
 import { ScheduleComponent } from './schedule/schedule.component';
@@ -37,8 +35,6 @@ import {MatButtonModule} from "@angular/material/button";
 @NgModule({
   declarations: [
     AdminComponent,
-    AdminNavbarComponent,
-    AdminSidenavComponent,
     LeaguageComponent,
     TeamComponent,
     ScheduleComponent,
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,10 @@
 import {Component, ViewChild} from '@angular/core';
-import {NgbCarousel, NgbSlideEvent, NgbSlideEventSource} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css',
-  
+
   ]
 })
 export class HomeComponent {
