875099526a3beb8919c2f39de863d425f5e4af26
tungdao <tung1602002@gmail.com>
2024-05-28 23:05:14 +0700

    reponsive side nav

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,12 +1,24 @@
-import {Component, computed, signal} from '@angular/core';
+import {Component, computed, OnInit, signal} from '@angular/core';
+import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
-export class AdminComponent {
+export class AdminComponent implements OnInit{
   collapsed = signal(false);
 
   sideNavWidth = computed(() => this.collapsed() ? '50px' : '200px')
+  constructor(private breakpointObserver: BreakpointObserver) { }
+  ngOnInit() {
+    this.breakpointObserver.observe([Breakpoints.Handset])
+      .subscribe(result => {
+        if (result.matches) {
+          this.collapsed.set(true);
+        } else {
+          this.collapsed.set(false);
+        }
+      });
+  }
 }
